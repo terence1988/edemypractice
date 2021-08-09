7ace30f42a94501d757aa6b8a051c1dc2e563174
Terence MA <terence.ma.lsu@gmail.com>
2021-08-10 08:29:09 +1000

    image upload

--- client/components/forms/CourseCreateForm.tsx
+++ client/components/forms/CourseCreateForm.tsx
@@ -2,6 +2,7 @@ import { Button, Divider, Select } from "antd";
 import { SaveOutlined } from "@ant-design/icons";
 import { CourseMetaData } from "../../types/Course";
 import { ChangeEventHandler, FormEventHandler } from "react";
+import Avatar from "antd/lib/avatar/avatar";
 
 const CourseCreateForm = ({
 	handleSubmit,
@@ -98,6 +99,8 @@ const CourseCreateForm = ({
 							<input type="file" name="image" onChange={handleImage} accept="image/*" hidden />
 						</label>
 					</div>
+					{preview && <Avatar style={{ width: "200" }} src={preview} />}
+					{//preview is on src so it must only took local filepath instead of online one}
 					<div className="row">
 						<div className="col">
 							<Button
--- client/pages/instructor/course/create.tsx
+++ client/pages/instructor/course/create.tsx
@@ -8,9 +8,11 @@ import {
 	SyntheticEvent,
 } from "react";
 import axios from "axios";
+import Resizer from "react-image-file-resizer";
 import InstructorRoute from "../../../components/routes/InstructorRoute";
 import CourseCreateForm from "../../../components/forms/CourseCreateForm";
 import { CourseMetaData } from "../../../types/Course";
+import { toast } from "react-toastify";
 
 const CreateCourse = () => {
 	const [courseMetaData, setCourseMetaData] = useState<CourseMetaData>({
@@ -24,6 +26,7 @@ const CreateCourse = () => {
 	});
 
 	const [preview, setPreview] = useState("");
+	const [uploadText, setUploadText] = useState("");
 
 	const handleOnChange: ChangeEventHandler<any> = (
 		e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -33,7 +36,25 @@ const CreateCourse = () => {
 
 	//most generic/base type SyntheticEvent
 	const handleImage: ChangeEventHandler<any> = (e: SyntheticEvent) => {
-		setPreview(window.URL.createObjectURL((e.currentTarget as HTMLInputElement).files[0]));
+		let file = (e.currentTarget as HTMLInputElement).files[0];
+		setPreview(window.URL.createObjectURL(file));
+		setUploadText(file.name);
+		setCourseMetaData({ ...courseMetaData, loading: true });
+
+		//resize image and store url in database
+		Resizer.imageFileResizer(file, 720, 500, "JPEG", 100, 0, async (uri) => {
+			try {
+				let { data } = await axios.post("api/corse/upload-image", {
+					image: uri,
+				});
+				console.log(data);
+				setCourseMetaData({ ...courseMetaData, loading: false });
+			} catch (err) {
+				console.log(err);
+				setCourseMetaData({ ...courseMetaData, loading: false });
+				toast("Image upload failed. Try later");
+			}
+		});
 	};
 
 	const handleSubmit: FormEventHandler<any> = (e: FormEvent) => {
