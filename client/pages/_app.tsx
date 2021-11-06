1bc03bbf68b48755096e6d1abccfb13cc8165b1e
mt1988 <awstest135@gmail.com>
2021-11-06 20:46:01 +1100

    Find an auth solution

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -9,48 +9,33 @@ import { UserContext, UserProvider } from "../contexts";
 //
 import { ToastContainer } from "react-toastify";
 import "../node_modules/react-toastify/dist/ReactToastify.css";
-import { IUser } from "@Itypes/User";
-import { Hidden } from "@mui/material";
 //context
 
-const AppWrapper = ({ children }) => {
-	// frame alwyas thinks all uppercase functions are components
-	const { state, dispatch } = useContext(UserContext);
-	const router = useRouter();
-
-	const [auth, setAuth] = useState<unknown>();
-
+function MyApp({ Component, pageProps }) {
+	// https://stackblitz.com/edit/listen-to-changes-in-localstorage-c8mzxn-khk6k6?file=index.tsx
 	useEffect(() => {
+		//React is probably not available
 		window.addEventListener("storage", () => {
 			const user = localStorage.getItem("x-next-user");
+			//React hooks are not available
 			const logout = async () => {
-				dispatch({ type: "LOGOUT" });
 				window.localStorage.removeItem("x-next-user");
 				const { data } = await axios.get("/api/logout");
-				console.log("Log out user from wrapper");
-				router.push("/login");
+				localStorage.setItem('Hook_logoutLog',JSON.stringify(data))
+				window.location.assign("/login");
 			};
-			setAuth(user);
 			if (!user) {
 				logout();
 			}
 		});
 	}, []);
-// https://stackblitz.com/edit/listen-to-changes-in-localstorage-c8mzxn-khk6k6?file=index.tsx
-	return (
-		<div>
-			{children}
-		</div>
-	);
-};
 
-function MyApp({ Component, pageProps }) {
 	return (
 		<UserProvider>
 			{/* Need to pass props down the line */}
-				<ToastContainer position={"top-center"} />
-				<TopNav />
-				<Component {...pageProps} />WW
+			<ToastContainer position={"top-center"} />
+			<TopNav />
+			<Component {...pageProps} />
 		</UserProvider>
 	);
 }
